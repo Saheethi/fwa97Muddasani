372c4d87e13e4feb34935f53ae0e1cfe68bb3a3f
Saheethi <89166758+Saheethi@users.noreply.github.com>
2021-10-27 22:12:00 -0500

    fixed errors in mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sahithi Muddasani' });
+  res.render('mydata', { title: 'Sahithi Muddasani' });
 });
 
 module.exports = router;
