b31fe1736d23c354f6f3855b419912de1abb0216
Saheethi <89166758+Saheethi@users.noreply.github.com>
2021-10-27 22:06:47 -0500

    fixed error apps.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var indexRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/mydata');
+app.use('/mydata',mydataRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
