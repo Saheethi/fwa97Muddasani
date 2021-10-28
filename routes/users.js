f1820269371b06e4088145a34de0d8a6e4211f80
Saheethi <89166758+Saheethi@users.noreply.github.com>
2021-10-27 22:48:07 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var n = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  n=n+1;
+  res.send(`User accesses are: ${n}`);
 });
 
 module.exports = router;
